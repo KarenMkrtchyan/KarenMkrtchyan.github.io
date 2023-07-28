ee6862612aea9c3e02ce7f8b1e947f0334847880
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2023-07-28 14:29:35 -0700

    CSS and HTML done

--- lambda.py
+++
@@ -1,9 +0,0 @@
-people=[
-    {"name":"Harry Spotter", "house":"GYMindor"},
-    {"name":"Cho", "house":"Ravenclaw"},
-    {"name":"Draco", "house": "Slytherin"}
-]
-
-people.sort(key=lambda person: person["name"])
-
-print(people)
